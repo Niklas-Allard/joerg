392228273edaeb25341d8a8330dea76c347a72b9
Niklas Allard <niklas.allard@web.de>
2025-06-02 23:13:14 +0200

    Feature: Shuffle in shuffle.php finished

---
+++ shuffle.php
@@ -0,0 +1,72 @@
+<?php 
+require "transforming_user_data.php";
+function shuffle_files($dir_name, $user_data) {
+
+    $shuffle = loading_user_data("shuffle.json"); // stores the next possible shuffle file
+
+    if (!isset($shuffle[$dir_name]) || count($shuffle[$dir_name]) == 0) {
+        $all_files = scandir($dir_name);
+
+        if ($all_files === false) {
+            require "log.php";
+            $log = new Log();
+            $log->saving_log("Error: Unable to read directory: " . $dir_name);
+        }
+
+        $allowed_files_types = loading_user_data("allowed_file_types.json");
+
+        $new_shuffle = array();
+
+        print_r($all_files);
+        print_r($allowed_files_types);
+
+        foreach ($all_files as $file) {
+            foreach ($allowed_files_types["movie"] as $type) {
+                if (str_ends_with($file, $type)) {
+                    $new_shuffle[] = $file;
+                }
+            }
+            foreach ($allowed_files_types["audio"] as $type) {
+                if (str_ends_with($file, $type)) {
+                    $new_shuffle[] = $file;
+                } 
+            }
+        }
+
+        print_r($new_shuffle);
+        
+        $shuffle[$dir_name] = $new_shuffle;
+
+        saving_user_data($shuffle, "shuffle.json");
+
+        $user_data["current_file"] = array_rand($shuffle[$dir_name]);
+        saving_user_data($user_data, "user_data.json");
+    } else {
+        $random_file_name = array_rand($shuffle[$dir_name]);
+
+        $user_data["current_file"] = $shuffle[$dir_name][$random_file_name];
+        saving_user_data($user_data, "user_data.json");
+
+        // Entferne die Datei aus dem Shuffle-Array
+        if (isset($shuffle[$dir_name][$random_file_name])) {
+            unset($shuffle[$dir_name][$random_file_name]); // Lösche das Element
+        }
+
+        saving_user_data($shuffle, "shuffle.json");
+
+        echo $user_data["current_file"];
+    }
+}
+
+shuffle_files("H:\\Serien\\Die Fraggles", loading_user_data("user_data.json"));
+
+/*
+Warning: Undefined array key "H:\Hoerspiele\5 Freunde" in C:\xampp\htdocs\joerg\shuffle.php on line 34
+
+Fatal error: Uncaught TypeError: array_rand(): Argument #1 ($array) must be of type array, null given in C:\xampp\htdocs\joerg\shuffle.php:34
+Stack trace:
+#0 C:\xampp\htdocs\joerg\shuffle.php(34): array_rand(NULL)
+#1 C:\xampp\htdocs\joerg\shuffle.php(39): shuffle_files('H:\\Hoerspiele\\5...', Array)
+#2 {main}
+  thrown in C:\xampp\htdocs\joerg\shuffle.php on line 34
+*/
