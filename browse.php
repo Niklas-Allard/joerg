f82d58e47127187eacd8339247ec18bea9d5bc02
Niklas Allard <niklas.allard@web.de>
2025-07-06 14:23:33 +0200

    Bug: resume icon is shown now

--- browse.php
+++ browse.php
@@ -257,7 +257,7 @@ body {
         <li>
           <!-- a single icon with an formulare -->
           <form action="browse.php" method="get">
-            <label for="resume"><svg width="<?php echo $icon_size; ?>" viewBox="http://www.w3.org/2000/svg" fill="white"><path d="M240-240v-480h80v480h-80Zm160 0 400-240-400-240v480Zm80-141v-198l165 99-165 99Zm0-99Z"/></svg></label>
+            <label for="resume"><svg xmlns="http://www.w3.org/2000/svg" width="<?php echo $icon_size; ?>" viewBox="0 -960 960 960" fill="white"><path d="M240-240v-480h80v480h-80Zm160 0 400-240-400-240v480Zm80-141v-198l165 99-165 99Zm0-99Z"/></svg></label>
             <input type="hidden" name="category" value="resume">
             <button type="submit" id="resume" class="submit" name="submit" value="submit"></button>
           </form>               
